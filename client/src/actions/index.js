c43f6c587cb29996d17664912239ae2525091930
sina <sina.akbari1996@gmail.com>
2019-02-28 16:44:09 +0330

    Updated DeleteStream, Added Modal to DeleteStream via React Portal

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -62,5 +62,6 @@ export const deleteStream = id => async dispatch => {
     type: DELETE_STREAM,
     payload: id
   })
+  history.push('/')
 }
 
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { fetchStream, deleteStream } from '../../actions'
+import history from '../../history'
+import Modal from '../Modal'
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component{
 
-  return(
-    <div>StreamDelete</div>
-  )
-};
-export default StreamDelete;
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  renderActions(){
+    const { id } = this.props.match.params
+    return(
+      // <React.Fragment></React.Fragment>
+      <>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">Cancel</Link>
+      </>
+    )
+  }
+
+  renderContent(){
+    if(!this.props.stream){
+      return 'Are You Sure You Want To Delete This Stream?'
+    }
+    return `Are You Sure You Want To Delete The Stream With Title: ${this.props.stream.title}`
+  }
+
+  render(){
+    
+    return(
+      <Modal
+      title="Delete Stream"
+      content={this.renderContent()}
+      actions={this.renderActions()}
+      onDismiss={() => history.push('/')}
+      />
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps,{ fetchStream, deleteStream})(StreamDelete)
+
+
+
+
